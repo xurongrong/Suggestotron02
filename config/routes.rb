a47b50f1d8621756ce887ea9333151e1d353cac8
xurongrong <xurongrong0821@gmail.com>
2017-03-03 16:17:12 +0800

    downvote, about

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  resources :topics
+  root 'welcome#index'
+  resources :topics do
+    member do
+      post 'upvote'
+      post 'downvote'
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
